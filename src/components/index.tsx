d5b66c8883201f38fbebebe17bb6781bfec27033
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-06-01 20:01:02 -0300

    chore: organizing the index for components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,11 @@
-export { CustomCircularProgress } from "./widget/CustomCircularProgress";
+// layout
 export { NavBar } from "./layout";
+
+// pages
 export { Pokedex } from "./pages/main/Pokedex";
 export { default as PokemonDetails } from "./pages/main/PokemonDetails";
+
+// widgets
 export { default as CustomCardFlip } from "./widget/CustomCardFlip";
+export { default as CustomGridItem } from "./widget/CustomGridItem";
+export { CustomCircularProgress } from "./widget/CustomCircularProgress";
