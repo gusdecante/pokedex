257c804c9d926740883dddad0f41ced67d147e7c
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-06-02 23:32:43 -0300

    refactor: changing home page to receive pokemons from statitProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,45 @@
+import { Typography } from "@mui/material";
+import { GetStaticProps } from "next";
 import type { NextPage } from "next";
 
 import { Pokedex } from "../components";
+import axios from "axios";
+import { IMAGE_API_URL, POKEMON_API_URL } from "../core/config";
+import { PokemonDataProps } from "../components/pages/main/Pokedex/Pokedex";
 
-const Home: NextPage = () => {
+type Props = {
+  author: string;
+  pokemons: PokemonDataProps[];
+};
+
+const Home: NextPage<Props> = ({ author, pokemons }) => {
   return (
     <div>
-      <Pokedex />
+      <Pokedex pokemons={pokemons} />
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const { data } = await axios.get(`${POKEMON_API_URL}?limit=100`);
+
+  const copyResult = data.results;
+
+  const pokemons: PokemonDataProps[] = copyResult.map(
+    (pokemon: any, index: number) => ({
+      id: index,
+      url: `${IMAGE_API_URL}${index + 1}.png`,
+      name: pokemon.name,
+    })
+  );
+
+  return {
+    props: {
+      author: "gusdecante",
+      pokemons,
+    },
+    revalidate: 60 * 60 * 60 * 24,
+  };
 };
 
 export default Home;
