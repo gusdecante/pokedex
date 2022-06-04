7e42d12cecb4bef20bee9579305a23592fb79538
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-06-03 21:59:36 -0300

    chore: removing grid item from card

---
+++ src/components/pages/main/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Grid, Card, CardMedia, CardContent, Typography } from "@mui/material";
+import Router from "next/router";
+
+import { useStyles } from "./styles";
+
+interface IPokeomnCard {
+  id: number;
+  name: string;
+  image: string;
+}
+
+export const PokemonCard: React.FC<IPokeomnCard> = ({ id, name, image }) => {
+  const { classes } = useStyles();
+
+  return (
+      <Card
+        className={classes.cardRoot}
+        onClick={() => Router.push(`pokemon/${id}`)}
+      >
+        <CardMedia className={classes.cardMedia} image={image} />
+        <CardContent>
+          <Typography className={classes.cardContent}>{name}</Typography>
+        </CardContent>
+      </Card>
+  );
+};
+
+export default PokemonCard;
