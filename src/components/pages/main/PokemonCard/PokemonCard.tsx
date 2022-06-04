767c525ac369c2216ac8cfa6fc974ce1c4ae745f
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-06-04 20:23:24 -0300

    chore: removing unused imports

--- src/components/pages/main/PokemonCard/PokemonCard.tsx
+++ src/components/pages/main/PokemonCard/PokemonCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Card, CardMedia, CardContent, Typography } from "@mui/material";
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
 import Router from "next/router";
 
 import { useStyles } from "./styles";
@@ -14,15 +14,15 @@ export const PokemonCard: React.FC<IPokeomnCard> = ({ id, name, image }) => {
   const { classes } = useStyles();
 
   return (
-      <Card
-        className={classes.cardRoot}
-        onClick={() => Router.push(`pokemon/${id}`)}
-      >
-        <CardMedia className={classes.cardMedia} image={image} />
-        <CardContent>
-          <Typography className={classes.cardContent}>{name}</Typography>
-        </CardContent>
-      </Card>
+    <Card
+      className={classes.cardRoot}
+      onClick={() => Router.push(`pokemon/${id}`)}
+    >
+      <CardMedia className={classes.cardMedia} image={image} />
+      <CardContent>
+        <Typography className={classes.cardContent}>{name}</Typography>
+      </CardContent>
+    </Card>
   );
 };
 
