9c7996402d54b781e4794d1b0fbda708ca7c4d2c
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2022-06-05 23:49:11 -0300

    fix: removing some styles from pokedex

--- src/components/pages/main/Pokedex/styles.ts
+++ src/components/pages/main/Pokedex/styles.ts
@@ -2,16 +2,7 @@ import { makeStyles } from "tss-react/mui";
 import { Theme } from "@mui/material";
 
 export const useStyles = makeStyles()((theme: Theme) => ({
-  pokedexRoot: {
-    "&.MuiBox-root": {
-      marginTop: theme.spacing(7),
-      display: "flex",
-      justifyContent: "center",
-    },
-  },
-  pokedexGridContainer: {
-    textAlign: "left",
-    padding: theme.spacing(2),
-    backgroundColor: "rgb(68, 68, 68)",
+  pokedexContainer: {
+    marginTop: theme.spacing(10),
   },
 }));
